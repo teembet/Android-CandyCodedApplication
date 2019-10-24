f416c8d5fbfff0827bc892fd6c7e4eb85b3868c5
teembet <timbetekanem@gmail.com>
2019-10-24 22:27:48 +0100

    second task

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -36,6 +36,15 @@ public void createMapIntent(View view){
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
+    public void createMapIntent(View view){
+        Uri uriAddress =Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent=new Intent(Intent.ACTION_VIEW,uriAddress);
+        mapIntent.setPackage("com.google.android.maps");
+
+        if (mapIntent.resolveActivity(getPackageManager())!=null){
+            startActivity(mapIntent);
+        }
+    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
